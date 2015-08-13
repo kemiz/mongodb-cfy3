f4825424cf16498ed63e3bdda6cb3a09413f2316
kemiz <kemimeister@gmail.com>
2015-08-13 12:18:05 +0100

    mongo db dproxy support

--- scripts/mongo/set-mongo-url.sh
+++ scripts/mongo/set-mongo-url.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ctx source instance runtime_properties mongo_ip_address $(ctx target instance host_ip)
-ctx source instance runtime_properties mongo_port $(ctx target node properties port)
+ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties outputs.endpoint.value.ip_address)
+ctx source instance runtime_properties mongo_port $(ctx target instance runtime_properties outputs.endpoint.value.port)
